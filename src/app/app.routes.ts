35bfa9fb55543efd0fa60b1899c9afb5b1568b81
dominikdeimel <info@dominikdeimel.com>
2026-02-21 23:20:03 +0100

    added fallback route handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('@/pages/generator/generator').then(m => m.Generator),},
+  { path: '', loadComponent: () => import('@/pages/generator/generator').then(m => m.Generator),},{
+    path: '**',
+    redirectTo: '',
+  },
+
 ];
