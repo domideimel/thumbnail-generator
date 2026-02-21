af35c52ade066d4e3f35c403fdc928b955c39d1b
dominikdeimel <info@dominikdeimel.com>
2026-02-21 20:48:39 +0100

    Enhance header with app logo and optimized image support

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterLink, RouterOutlet } from '@angular/router'
 import { LayoutImports } from '@/shared/components/layout'
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutImports, RouterLink],
+  imports: [RouterOutlet, LayoutImports, RouterLink, NgOptimizedImage],
   templateUrl: './app.html',
 })
 export class App {
