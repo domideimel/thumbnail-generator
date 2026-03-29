8f0c6f2bc165d774053295a5f99b5ee62d5a568c
dominikdeimel <info@dominikdeimel.com>
2026-03-29 19:47:52 +0200

    Add schema validation for `YoutubeMetadataModel` in `thumbnail-generator.service`
    - Introduced `YoutubeMetadataModelSchema` using `valibot` for structured validation.
    - Replaced manual metadata checks with `parse()` to ensure strict type validation.
    - Updated `youtube-metadata.model.ts` to define and export the validation schema.

--- src/app/models/youtube-metadata.model.ts
+++ src/app/models/youtube-metadata.model.ts
@@ -1,22 +1,28 @@
-export interface YoutubeMetadataModel {
-  provider_url: string
-  thumbnail_height: number
-  thumbnail_width: number
-  html: string
-  thumbnail_url: string
-  width: number
-  url: string
-  version: string
-  type: string
-  title: string
-  author_name: string
-  provider_name: string
-  height: number
-  author_url: string
-}
+import { InferOutput, number, object, string } from 'valibot'
 
 export interface GeneratedImage {
   filename: string
   dataUrl: string
   type: 'full' | 'large' | 'basic' | 'blurred'
 }
+
+export const YoutubeMetadataModelSchema = object({
+  provider_url: string(),
+  thumbnail_height: number(),
+  thumbnail_width: number(),
+  html: string(),
+  thumbnail_url: string(),
+  width: number(),
+  url: string(),
+  version: string(),
+  type: string(),
+  title: string(),
+  author_name: string(),
+  provider_name: string(),
+  height: number(),
+  author_url: string(),
+})
+
+export type YoutubeMetadataModel = InferOutput<
+  typeof YoutubeMetadataModelSchema
+>
